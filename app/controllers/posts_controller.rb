3d958c998d4a71c1a8e587278f5ef23e7f78bbaa
Zil Norvilis <zilasino27@gmail.com>
2021-04-13 16:24:40 +0200

    add authorization to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user = current_user
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'post was created' }
--- db/migrate/20210413134850_devise_create_users.rb
+++ db/migrate/20210413134850_devise_create_users.rb
@@ -14,6 +14,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       ## Rememberable
       t.datetime :remember_created_at
 
+      # Custom fields
+      t.string :name
+
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
